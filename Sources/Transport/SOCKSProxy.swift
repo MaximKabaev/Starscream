ee9c8fbbeaff5e1646c1643ece4e42ee2a548dc9
Maxim <albinauric21@gmail.com>
2026-04-10 21:23:16 +0300

    Add user/password support to SOCKS proxy config

--- Sources/Transport/SOCKSProxy.swift
+++ Sources/Transport/SOCKSProxy.swift
@@ -13,19 +13,28 @@ import Foundation
 public struct SOCKSProxyConfig {
     public let host: String
     public let port: Int
+    public let user: String?
+    public let password: String?
 
-    public init(host: String, port: Int) {
+    public init(host: String, port: Int, user: String? = nil, password: String? = nil) {
         self.host = host
         self.port = port
+        self.user = user
+        self.password = password
     }
 
     /// Creates a stream configuration callback that sets up SOCKS proxy on the streams
     public func streamConfiguration() -> ((InputStream, OutputStream) -> Void) {
-        return { [host, port] inStream, outStream in
-            let proxyConfig: [String: Any] = [
+        return { [host, port, user, password] inStream, outStream in
+            var proxyConfig: [String: Any] = [
                 kCFStreamPropertySOCKSProxyHost as String: host,
                 kCFStreamPropertySOCKSProxyPort as String: port
             ]
+
+            if let user, let password {
+                proxyConfig[kCFStreamPropertySOCKSUser as String] = user
+                proxyConfig[kCFStreamPropertySOCKSPassword as String] = password
+            }
 
             let propertyKey = CFStreamPropertyKey(rawValue: kCFStreamPropertySOCKSProxy)
             let cfProxyConfig = proxyConfig as CFDictionary
